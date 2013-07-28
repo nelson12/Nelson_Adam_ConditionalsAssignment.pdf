0f34cac2e737a23ec3f38d59ebbe9e38ab45d5a3
nelson12# <aw.nelson@hotmail.com>
2013-07-28 14:50:49 -0400

    First variable and comment

---
+++ Industry Conditional copy.js
@@ -0,0 +1,4 @@
+//adam nelson 072813 Industry Conditionals 
+//Should you get your incision checked after surgery?
+
+var temp= prompt("I am trying to see if you need to have your incision looked at after surgery What is your temperature?");//Declaring variable
