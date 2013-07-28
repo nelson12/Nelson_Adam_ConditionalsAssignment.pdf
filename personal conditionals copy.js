31ef0bbeb2e778a5927802d166db006050a2d503
nelson12# <aw.nelson@hotmail.com>
2013-07-28 14:21:34 -0400

    declaring variable personal
    declaring my two variables for personal assignment

---
+++ personal conditionals copy.js
@@ -0,0 +1,5 @@
+//adam nelson 072813 Personal Conditionals 
+//
+
+var guests= prompt("I am trying to figure out how many cookies to make, how many guests will be at your party?");//declaring variable
+var hours= prompt("How long will the party be?");//declaring variable
