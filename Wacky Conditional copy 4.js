33dc203f88304690c14cc67bfd4847703482556b
nelson12# <aw.nelson@hotmail.com>
2013-07-28 15:25:07 -0400

    What to do if true

---
+++ Wacky Conditional copy 4.js
@@ -0,0 +1,7 @@
+//adam nelson 072813 Wacky Conditionals 
+//How many drunk are you?
+
+var tolerance= prompt("Im determining how drunk you are, How many beers does it take to give you a buzz?");//declaring first variable
+var beers=prompt("How many drinks have you had tonight?");//declaring second variable
+
+(tolerance>2 || beers> 6) ? console.log("Your as think as you drunk you are!") //stating what to do if true
