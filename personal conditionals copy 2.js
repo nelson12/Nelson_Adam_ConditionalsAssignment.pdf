6fd593adff6567ea6607e24737dee585c69f1ae3
nelson12# <aw.nelson@hotmail.com>
2013-07-28 14:24:31 -0400

    If statement
    my first part of else if statement

---
+++ personal conditionals copy 2.js
@@ -0,0 +1,8 @@
+//adam nelson 072813 Personal Conditionals 
+//How many cookies must be made for a party?
+
+var guests= prompt("I am trying to figure out how many cookies to make, how many guests will be at your party?");//declaring variables
+var hours= prompt("How long will the party be?");//declaring variables
+
+if (guests>12 && hours>2){
+	console.log("Double the recipe!");//making an if statement
