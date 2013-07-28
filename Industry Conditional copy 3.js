f9085227d2f5f6ed17df876819b0d455b0095c95
nelson12# <aw.nelson@hotmail.com>
2013-07-28 14:59:26 -0400

    Settting Parameters

---
+++ Industry Conditional copy 3.js
@@ -0,0 +1,7 @@
+//adam nelson 072813 Industry Conditionals 
+//Should you get your incision checked after surgery?
+
+var temp= prompt("I am trying to see if you need to have your incision looked at after surgery What is your temperature?");//Declaring variable
+var pain= prompt("On a scale of one to ten, What is your pain level at?");//declaring variable
+
+(temp>102 || pain>6) //setting parameters
