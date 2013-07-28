5b59477035a2b27736f38b1e9d3b303efdd1fe5c
nelson12# <aw.nelson@hotmail.com>
2013-07-28 15:17:26 -0400

    Second Variable

---
+++ Wacky Conditional copy 2.js
@@ -0,0 +1,5 @@
+//adam nelson 072813 Wacky Conditionals 
+//How many drunk are you?
+
+var tolerance= prompt("Im determining how drunk you are, How many beers does it take to give you a buzz?");//declaring first variable
+var beers=prompt("How many drinks have you had tonight?");//declaring second variable
